cb121ac9093aefd765148e1dd33b05e1c577273a
Salma sherif <40717302+Salmasherif33@users.noreply.github.com>
2021-12-23 22:58:24 +0200

    add show store API

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -10,5 +10,35 @@ use Illuminate\Support\Facades\DB;
 
 class StoreController extends Controller
 {
- 
+ public function show($UserID){
+        if(!$UserID){
+            return response()->json([
+                'message' =>'store id is required'
+            ],244);
+        }
+        
+        if(User::where('id','=',$UserID)->exists()){
+        $info = User::where('id','=',$UserID)
+        ->select('users.image as userImage','users.Storename','users.name as userName','users.email')
+        ->first();
+        
+        $items = Item::select('items.*')->where('owner_id','=',$UserID)->get();
+        $sold_items = DB::table('sellers')->where('sellers.seller_id', '=', $UserID)
+        ->join('items', 'sellers.item_id', '=', 'items.id')
+        ->join('users', 'items.owner_id','=', 'users.id')
+        ->select ('items.*' , 'users.Storename')
+        ->get();
+        //return view('user.StoreScreen',['info'=>$info, 'items'=>$items ,'sold_items'=>$sold_items, 'user'=>$user , 'id'=>$UserID ]);
+        return response()->json([
+            'info of the store' => $info,
+            'items'=> $items,
+            'sold_items of other stores' => $sold_items
+        ]);
+    }
+    else{
+        return response()->json([
+            'message' => 'there is no such a store'
+        ]);
+    }
+    }
 }
