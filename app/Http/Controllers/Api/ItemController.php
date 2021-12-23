a17bd2328a8a149d78eeb31ea7473488dd917bfb
nadinemostafa1-9 <73845665+nadinemostafa1-9@users.noreply.github.com>
2021-12-23 11:20:17 +0200

    Update ItemController.php

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -12,5 +12,90 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
+//add new product
+    public function store(Request $request){
+        if (auth()->user()) {
+            $user = auth()->user();
+            $fields = $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'description' => ['required' , 'string'],
+                'price' => ['required' ,'integer'],
+                'amount' => ['required' , 'integer']
+            ]);
+            $item = new Item;
+            $item->name = $fields['name'];
+            $item->price =  $fields['price'];
+            $item->amount = $fields['amount'];
+            $item->description = $fields['description'];
+            $item->owner_id = $user->id;
+        
+        if ($request->hasFile('image')) {
+                    $file = $request->file('image');
+                    $exten = $file->getClientOriginalExtension();
+                    $filename = time() . '.' . $exten;
+                    $file->move('upload/items/', $filename);
+                    $item->image = $filename;
+                }
 
+                $item->save();
+                $reponse = [
+                    'message' => 'success , The item is added successfully',
+                    'item'=> $item
+                ];
+            $op  = 'store';
+            $this->update_report($op, $item,$user);
+            return  response()->json($reponse, 201);
+            }
+    }
+    //update product
+    public function Update(Request $request)
+    {      
+        $user = auth()->user();
+        $fields = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['required' , 'string'],
+            'price' => ['required' ,'integer'],
+            'amount' => ['required' , 'integer'],
+            'id' =>['required']
+        ]);
+        if (Item::where('id', $fields['id'])->exists()) {
+            $item = Item::where('id', $fields['id'])->first();
+            if($item->owner_id == $user->id){
+            $item->name = $fields['name'];
+            $item->price =  $fields['price'];
+            $item->amount = $fields['amount'];
+            $item->description = $fields['description'];
+            $item->owner_id = $user->id;
+            if ($request->hasFile('image')) {
+              
+                $file = $request->file('image');
+                $exten = $file->getClientOriginalExtension();
+                $filename = time() . '.' . $exten;
+                $file->move('upload/items/', $filename);
+                $item->image = $filename;
+            }
+            $item->save();
+            $reponse = [
+                'message' => 'success , The item is updated successfully',
+                'item'=> $item
+            ];
+            $op  = 'update';
+            $this->update_report($op, $item,$user);
+            return  response()->json($reponse, 201);
+             }
+
+        else  {
+            $reponse = [
+                'message: ' => 'fail this item doesn\'t belong to your store'
+            ];
+            return response()->json($reponse, 214);
+        } 
+        }
+        else{
+          $reponse = [
+                'message: ' => 'fail No such an item'
+            ];
+            return response()->json($reponse, 215);
+        }
+      
 }
